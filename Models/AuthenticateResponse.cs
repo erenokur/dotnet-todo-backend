6997ce45abccae6e273faca1e6c2c55fc01d9130
eren okur <32430029+erenokur@users.noreply.github.com>
2023-05-25 23:47:40 +0300

    SQL will be used.

--- Models/AuthenticateResponse.cs
+++ Models/AuthenticateResponse.cs
@@ -4,8 +4,8 @@ using dotnet_todo_backend.interfaces;
 public class AuthenticateResponse : IAuthenticateResponse
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Username { get; set; }
-    public string Token { get; set; }
+    public string FirstName { get; set; } = String.Empty;
+    public string LastName { get; set; } = String.Empty;
+    public string Username { get; set; } = String.Empty;
+    public string Token { get; set; } = String.Empty;
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -13,7 +13,6 @@ using dotnet_todo_backend.interfaces;
 
 public class UserService : IUserService
 {
-    // users hardcoded for simplicity, store in a db with hashed passwords in production applications
     private List<User> _users = new List<User>
     {
         new User { Id = 1, FirstName = "Test", LastName = "User", Username = "test", Password = "test" }

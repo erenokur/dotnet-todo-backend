23648569be9556c30b326f091418911b064939dd
eren okur <32430029+erenokur@users.noreply.github.com>
2023-05-30 21:33:20 +0300

    gitignore changed

---
+++ db/MyDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+public class MyDbContext : DbContext
+{
+    public DbSet<MyEntity> MyEntities { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=database.db");
+    }
+}

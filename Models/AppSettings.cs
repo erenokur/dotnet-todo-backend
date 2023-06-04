4bdb7b128b3aae3bd21b3d6a4b069e0cd67ea400
erenokur <akkent1991@hotmail.com>
2023-06-04 15:39:16 +0300

    mongo db integration added. need to be testing.

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -3,5 +3,7 @@ namespace dotnet_todo_backend.Models;
 public class AppSettings
 {
     public string Secret { get; set; } = String.Empty;
+    public string ConnectionString { get; set; } = String.Empty;
+    public string DatabaseName { get; set; } = String.Empty;
 
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,75 +1,81 @@
-namespace dotnet_todo_backend.Services;
-
+using dotnet_todo_backend.Entities;
+using dotnet_todo_backend.Helpers;
+using dotnet_todo_backend.interfaces;
+using dotnet_todo_backend.Models;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
-using dotnet_todo_backend.Entities;
-using dotnet_todo_backend.Helpers;
-using dotnet_todo_backend.Models;
-using dotnet_todo_backend.interfaces;
 
-public class UserService : IUserService
+namespace dotnet_todo_backend.Services
 {
-    private List<User> _users = new List<User>
+    public class UserService : IUserService
     {
-        new User { id = "1", username = "test", email = "User", password = "test", created = DateTime.Now}
-    };
+        private readonly IMongoCollection<User> _users;
 
-    private readonly AppSettings _appSettings;
+        private readonly AppSettings _appSettings;
 
-    public UserService(IOptions<AppSettings> appSettings)
-    {
-        _appSettings = appSettings.Value;
-    }
-
-    public AuthenticateResponse? Authenticate(AuthenticateRequest model)
-    {
-        var user = _users.SingleOrDefault(x => x.username == model.Username && x.password == model.Password);
-
-        // return null if user not found
-        if (user == null) return null;
-
-        // authentication successful so generate jwt token
-        var tokenJson = generateJwtToken(user);
-
-        // serialize user and token to JSON strings
-        string userJson = JsonSerializer.Serialize(user);
-
-        AuthenticateResponse response = new AuthenticateResponse
+        public UserService(IOptions<AppSettings> appSettings)
         {
-            id = user.id,
-            username = user.username,
-            token = tokenJson
-        };
-        return response;
-    }
+            var client = new MongoClient(appSettings.Value.ConnectionString);
+            var database = client.GetDatabase(appSettings.Value.DatabaseName);
+            _users = database.GetCollection<User>("users");
 
-    public IEnumerable<User> GetAll()
-    {
-        return _users;
-    }
+            _appSettings = appSettings.Value;
+        }
 
-    public User? GetById(string id)
-    {
-        return _users.FirstOrDefault(x => x.id == id);
-    }
-
-    // helper methods
-
-    private string generateJwtToken(User user)
-    {
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-        var tokenDescriptor = new SecurityTokenDescriptor
+        public AuthenticateResponse? Authenticate(AuthenticateRequest model)
         {
-            Subject = new ClaimsIdentity(new[] { new Claim("id", user.id.ToString()) }),
-            Expires = DateTime.UtcNow.AddDays(7),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-        };
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-        return tokenHandler.WriteToken(token);
+            var user = _users.Find(x => x.username == model.Username && x.password == model.Password).SingleOrDefault();
+
+            // return null if user not found
+            if (user == null) return null;
+
+            // authentication successful so generate jwt token
+            var tokenJson = generateJwtToken(user);
+
+            // serialize user and token to JSON strings
+            string userJson = JsonSerializer.Serialize(user);
+
+            AuthenticateResponse response = new AuthenticateResponse
+            {
+                id = user.id,
+                username = user.username,
+                token = tokenJson
+            };
+            return response;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _users.Find(x => true).ToList();
+        }
+
+        public User? GetById(string id)
+        {
+            return _users.Find(x => x.id == id).SingleOrDefault();
+        }
+
+        // helper methods
+
+        private string generateJwtToken(User user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", user.id.ToString()) }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
     }
 }
